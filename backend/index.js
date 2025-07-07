b446167cbca114deafb6029a166e7f76786386bf
Amulya Reddy <samulya2006@gmail.com>
2025-07-07 14:32:04 +0530

    updated files related to student

--- backend/index.js
+++ backend/index.js
@@ -20,18 +20,17 @@ app.get("/", (req, res) => {
 
 app.use("/media", express.static(path.join(__dirname, "media")));
 
-app.use("/api/branch", require("./routes/branch.route"));
-app.use("/api/exam", require("./routes/exam.route"));
-app.use("/api/marks", require("./routes/marks.route"));
-app.use("/api/material", require("./routes/material.route"));
-app.use("/api/notice", require("./routes/notice.route"));
-app.use("/api/subject", require("./routes/subject.route"));
-app.use("/api/timetable", require("./routes/timetable.route"));
 app.use("/api/admin", require("./routes/details/admin-details.route"));
 app.use("/api/faculty", require("./routes/details/faculty-details.route"));
 app.use("/api/student", require("./routes/details/student-details.route"));
 
-
+app.use("/api/branch", require("./routes/branch.route"));
+app.use("/api/subject", require("./routes/subject.route"));
+app.use("/api/notice", require("./routes/notice.route"));
+app.use("/api/timetable", require("./routes/timetable.route"));
+app.use("/api/material", require("./routes/material.route"));
+app.use("/api/exam", require("./routes/exam.route"));
+app.use("/api/marks", require("./routes/marks.route"));
 
 app.listen(port, () => {
   console.log(`Server Listening On http://localhost:${port}`);
